6b2bcbd913bf9f43481b4426e0d0a33630b324d9
MohamedArshath10 <150664091+MohamedArshath10@users.noreply.github.com>
2024-02-24 14:56:19 +0530

    Add files via upload
    final project with lap and mobile responsive functions

---
+++ script.js
@@ -0,0 +1,13 @@
+function openNav(){
+    document.querySelector(".side-navbar").style.width = "50%";
+    document.querySelector(".side-navbar").style.visibility = "visible";
+}
+function closeNav(){
+    document.querySelector(".side-navbar").style.width = "0";
+    document.querySelector(".side-navbar").style.visibility = "hidden";
+}
+
+
+// header and go top button on page scroll
+
+let section = document.querySelector('')
